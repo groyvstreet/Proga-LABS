5a72ed9897415726b2d07faa1101541818b8b616
groyvstreet <75688738+groyvstreet@users.noreply.github.com>
2021-03-18 14:59:05 +0300

    Lab3

--- Lab3/main_test.c
+++ Lab3/main_test.c
@@ -16,33 +16,17 @@ void Test2CreateArr()
     assert(arr != NULL);
 }
 
-void Test3CreateArr()
-{
-    int **arr;
-    arr = CreateArr(arr, 10000, 10000);
-    assert(arr != NULL);
-}
-
-void Test4CreateArr()
-{
-    int **arr;
-    arr = CreateArr(arr, 10000000, 10000);
-    assert(arr == NULL);
-}
-
 void Test1CreateArr3D()
 {
     int ***arr;
-    arr = CreateArr3D(arr, 100, 100, 10);
-    assert(arr == NULL);
+    arr = CreateArr3D(arr, 100, 100, 20);
+    assert(arr != NULL);
 }
 
 int main()
 {
     Test1CreateArr();
     Test2CreateArr();
-    Test3CreateArr();
-    Test4CreateArr();
     Test1CreateArr3D();
     printf("Tests passed successfully!\n");
 
